739d3dcbce0780f0614842c1e5c86607f0153c21
Agustina Sambucetti <agustinasambucetti@airdeagustina2.lan>
2026-06-08 11:10:58 -0300

    elaboracion de Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,16 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import DynamicForm from '../components/DynamicForm';
 import { FlatList } from 'react-native-web';
 import { db } from '../firebase/config';
 
-// este falta chequear bien que pide y hacerlo
 function Home(props) {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
 
 
     useEffect(() => {
-        db.collection('posts').onSnapshot(
+        db.collection('posts').onSnapshot( //metodo onsnapshot logra que firebase entregue la info actulizada. coleccion de posts ya existe (fue creada en CrearPost)
             docs => {
                 let posts = [];
                 docs.forEach(doc => {
@@ -24,7 +23,12 @@ function Home(props) {
                 })
             }
         )
-    })
+    });
+
+
+    function onSubmit(props) {
+        props.navigation.navigate('Comentarios'); // futura screen donde usu podra agregar comentarios
+    }
 
 
     return (
@@ -32,11 +36,22 @@ function Home(props) {
             <Text>NOMBRE DE LA APP</Text>
             <DynamicForm />
             <FlatList
-                data={posts}
+                data={posts} // esto esta llamando a la const de arriba y esta linkeado a la screen de posts asi muestra todos y es scrolleable
                 keyExtractor={item => item.id.toString()}
-                renderItem={({ item }) => <Text>{item.title}</Text>}
+                renderItem={({ item }) =>// todo lo que vaya despues del data va a depender como lo defina mi compañera en crearPosts/posts
+                    <View>
+                        <Text>Nombre del usuario: {item.data.name}</Text>
+                        <Text>{item.data.img}</Text>
+                        <Text>{item.data.descripcion}</Text>
+                        <Text>{item.data.img}</Text>
+                        <Text>{item.data.likes}</Text>
+
+                        <Pressable onPress={() => onSubmit()}>
+                            <Text>Comment</Text>
+                        </Pressable>
+                    </View>} // en este atributo(renderItem) renderizamos cada cosa que me pide la consigna, los datos los saco de la const posts donde, entrando a data, entro al objeto literal con todos sus atributos (email, nombre, descripcion)
             />
-        </View>
+        </View >
     )
 }
 
