bbbcec6ea1cbed41b01c7021eecae33edc8b171b
Micaela Son <mica@Mac.fibertel.com.ar>
2026-06-20 15:50:43 -0300

    orden visual

--- src/components/NavegacionStackHome.js
+++ src/components/NavegacionStackHome.js
@@ -7,7 +7,7 @@ const Stack = createNativeStackNavigator();
 function NavegacionStackHome(){
     return(
     <Stack.Navigator>
-        <Stack.Screen name="Home" component={ Home } />
+        <Stack.Screen name="Home" component={ Home } options={ { headerShown: false } } />
         <Stack.Screen name="ComentarPosteo" component={ ComentarPosteo }/>
      </Stack.Navigator>
     )
